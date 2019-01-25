9d8ee4f79b08aa42cbd333ebd32ea7828af70726
Jared Johnson <jaredj@gmx.com>
2019-01-24 21:05:44 -0500

    Implemented some file explorer binds for oni

--- .config/oni/config.js
+++ .config/oni/config.js
@@ -1,29 +1,42 @@
 "use strict";
 exports.__esModule = true;
 exports.activate = function (oni) {
-    console.log("config activated");
+    var editors = oni.editors, menu = oni.menu;
+    var isVisualMode = function () { return editors.activeEditor.mode === "visual"; };
+    var isNormalMode = function () { return editors.activeEditor.mode === "normal"; };
+    var isNotInsertMode = function () { return editors.activeEditor.mode !== "insert"; };
+    var isInsertOrCommandMode = function () {
+        return editors.activeEditor.mode === "insert" || editors.activeEditor.mode === "cmdline_normal";
+    };
+    var oniWithSidebar = oni;
+    var isSidebarPaneOpen = function (paneId) {
+        return oniWithSidebar.sidebar.activeEntryId === paneId &&
+            oniWithSidebar.sidebar.isFocused &&
+            !isInsertOrCommandMode() &&
+            !isMenuOpen();
+    };
+    var isExplorerActive = function () { return isSidebarPaneOpen("oni.sidebar.explorer"); };
+    var isVCSActive = function () { return isSidebarPaneOpen("oni.sidebar.vcs"); };
+    var isMenuOpen = function () { return menu.isMenuOpen(); };
     // Input
-    //
-    // Add input bindings here:
-    //
-    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    //FileExplorer
+    oni.input.unbind("<space>");
+    oni.input.unbind("o");
     oni.input.unbind("<s-c-b>");
-    oni.input.bind("<space>d", "sidebar.toggle");
-    //
-    // Or remove the default bindings here by uncommenting the below line:
-    //
-    // oni.input.unbind("<c-p>")
-};
-exports.deactivate = function (oni) {
-    console.log("config deactivated");
+    oni.input.bind("<space>d", "explorer.toggle", isNotInsertMode);
+    oni.input.unbind("d");
+    oni.input.bind("md", "explorer.delete", isExplorerActive);
+    oni.input.unbind("r");
+    oni.input.bind("mm", "explorer.rename", isExplorerActive);
+    oni.input.unbind("<c-e>");
+    oni.input.bind("ma", "explorer.create.file", isExplorerActive);
+    oni.input.bind("o", "explorer.expand.directory", isExplorerActive);
+    oni.input.bind("o", "explorer.collapse.directory", isExplorerActive);
+    oni.input.unbind("<f12>");
+    oni.input.bind("gd", "language.gotoDefinition", function () { return isNormalMode() && !menu.isMenuOpen(); });
 };
 exports.configuration = {
     //add custom config here, such as
-    //"oni.useDefaultConfig": true,
-    //"oni.bookmarks": ["~/Documents"],
-    //"oni.loadInitVim": false,
-    //"editor.fontSize": "12px",
-    //"editor.fontFamily": "Monaco",
     // UI customizations
     "ui.animations.enabled": true,
     "ui.fontSmoothing": "auto",
--- .config/oni/config.tsx
+++ .config/oni/config.tsx
@@ -1,37 +1,56 @@
 import * as React from "react"
 import * as Oni from "oni-api"
 
+interface ISidebar {
+    sidebar: {
+        activeEntryId: string
+        isFocused: boolean
+    }
+}
+
 export const activate = (oni: Oni.Plugin.Api) => {
-    console.log("config activated")
+    const { editors, menu } = oni;
+
+	const isVisualMode = () => editors.activeEditor.mode === "visual";
+    const isNormalMode = () => editors.activeEditor.mode === "normal";
+    const isNotInsertMode = () => editors.activeEditor.mode !== "insert";
+    const isInsertOrCommandMode = () =>
+        editors.activeEditor.mode === "insert" || editors.activeEditor.mode === "cmdline_normal";
+
+    const oniWithSidebar = oni as Oni.Plugin.Api & ISidebar
+    const isSidebarPaneOpen = (paneId: string) =>
+        oniWithSidebar.sidebar.activeEntryId === paneId &&
+        oniWithSidebar.sidebar.isFocused &&
+        !isInsertOrCommandMode() &&
+        !isMenuOpen();
+
+    const isExplorerActive = () => isSidebarPaneOpen("oni.sidebar.explorer");
+    const isVCSActive = () => isSidebarPaneOpen("oni.sidebar.vcs");
+
+    const isMenuOpen = () => menu.isMenuOpen();
 
     // Input
-    //
-    // Add input bindings here:
-    //
-    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+    //FileExplorer
+    oni.input.unbind("<space>");
+    oni.input.unbind("o");
     oni.input.unbind("<s-c-b>");
-    oni.input.bind("<space>d", "sidebar.toggle");
+    oni.input.bind("<space>d", "explorer.toggle", isNotInsertMode);
+    oni.input.unbind("d");
+    oni.input.bind("md", "explorer.delete", isExplorerActive);
+    oni.input.unbind("r");
+    oni.input.bind("mm", "explorer.rename", isExplorerActive);
+    oni.input.unbind("<c-e>");
+    oni.input.bind("ma", "explorer.create.file", isExplorerActive);
+    oni.input.bind("o", "explorer.expand.directory", isExplorerActive);
+    oni.input.bind("o", "explorer.collapse.directory", isExplorerActive);
 
-
-    //
-    // Or remove the default bindings here by uncommenting the below line:
-    //
-    // oni.input.unbind("<c-p>")
-
+    oni.input.unbind("<f12>");
+    oni.input.bind("gd", "language.gotoDefinition", () => isNormalMode() && !menu.isMenuOpen())
 }
 
-export const deactivate = (oni: Oni.Plugin.Api) => {
-    console.log("config deactivated")
-}
 
 export const configuration = {
     //add custom config here, such as
-
-    //"oni.useDefaultConfig": true,
-    //"oni.bookmarks": ["~/Documents"],
-    //"oni.loadInitVim": false,
-    //"editor.fontSize": "12px",
-    //"editor.fontFamily": "Monaco",
 
     // UI customizations
     "ui.animations.enabled": true,
